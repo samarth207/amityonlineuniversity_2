f00f320ee6f0e749cd843d1fa19abd7e67eb7d28
samarth <samthedude4321@gmail.com>
2025-12-19 18:03:19 +0530

    addded

--- submit-form.php
+++ submit-form.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
